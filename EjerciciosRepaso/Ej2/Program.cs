26da8c47e4a5a830cee3a9172d388e1781693c1f
Eitan Lab <eitanlab@gmail.com>
2020-09-05 16:30:18 -0300

    Ejercicio 2 terminado

--- EjerciciosRepaso/Ej2/Program.cs
+++ EjerciciosRepaso/Ej2/Program.cs
@@ -10,6 +10,43 @@ namespace Ej2
     {
         static void Main(string[] args)
         {
+            int regNumber = 864;
+            for (int i = 1; i <= regNumber; i++)
+            {
+                switch (i)
+                {
+                    case var n when isPrime(n) & isMultiplier(n,3):
+                        Console.WriteLine(i + " - Es primo y divisible por 3");
+                        break;
+                    case var n when isMultiplier(n, 3):
+                        Console.WriteLine(i + " - Es divisible por 3");
+                        break;
+                    case var n when isPrime(n):
+                        Console.WriteLine(i + " - Es primo");
+                        break;
+                    default:
+                        Console.WriteLine(i);
+                        break;
+                }
+            }
+        }
+
+        static bool isPrime(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool isMultiplier(int number, int divisor)
+        {
+            if (number % divisor == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
