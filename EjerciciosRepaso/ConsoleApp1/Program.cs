c9e28ca51059f49b4eb2217333929b212998c123
Eitan Lab <eitanlab@gmail.com>
2020-09-06 14:36:14 -0300

    Ejercicio 3 terminado

---
+++ EjerciciosRepaso/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "anana";
+            string invertedWord = "";
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                invertedWord += word[i];
+            }
+            if (invertedWord == word)
+            {
+                Console.WriteLine(word + " es palíndromo");
+            } else
+            {
+                Console.WriteLine(word + " no es palíndromo");
+            }
+        }
+    }
+}
